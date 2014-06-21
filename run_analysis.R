09ab678f9353f60bdb63bdc966f71960c81086e3
MaryLuu <kleopatra069@gmail.com>
2014-06-21 09:01:32 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -6,7 +6,7 @@
 # Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
 # Good luck!
 
-setwd("C:/Users/PCPC/Desktop/Data-cleaning/getdata-projectfiles-UCI-HAR-Dataset")
+setwd("C:/Users/Administrator/Desktop/UCI HAR Dataset")
 getwd()
 
 ###
@@ -18,14 +18,12 @@ getwd()
 # reading data
 X_ts <- read.table("./test/X_test.txt")
 head(X_ts)
-
 X_tr <- read.table("./train/X_train.txt")
 head(X_tr)
 
 
 y_ts <- read.table("./test/y_test.txt")
 head(y_ts)
-
 y_tr <- read.table("./train/y_train.txt")
 head(y_tr)
 
@@ -37,13 +35,13 @@ s_ts <- read.table("./test/subject_test.txt")
 
 X_tr_ts <- rbind.data.frame(X_tr, X_ts)
 head(X_tr_ts)
-tail(X_tr_ts)
+
 dim(X_tr_ts)
 
 
 Y_tr_ts <- rbind.data.frame(y_tr, y_ts)
 head(Y_tr_ts)
-tail(Y_tr_ts)
+
 dim(Y_tr_ts)
 
 s <- rbind(s_tr, s_ts)
@@ -54,10 +52,12 @@ Fe <- read.table("./features.txt")
 head(Fe)
 
 
-MeanSD_X_tr_ts <- grep("-mean\\(\\)|-std\\(\\)", Fe$V2)
-x.mean.sd <- X_tr_ts[ ,MeanSD_X_tr_ts]
+Mean_SD <- grep("-mean\\(\\)|-std\\(\\)", Fe$V2)
+X_Mean_Sd <- X_tr_ts[ ,Mean_SD]
 
-head(x.mean.sd)
+names(X_Mean_Sd) <- Fe[Mean_SD, 2]
+names(X_Mean_Sd) <- gsub("\\(|\\)", "", names(X_Mean_Sd))
+head(X_Mean_Sd)
 
 # Mean_X_tr_ts <- sapply(X_tr_ts, mean)
 # head(Mean_X_tr_ts)
@@ -68,20 +68,21 @@ head(x.mean.sd)
 
 activ <- read.table('./activity_labels.txt')
 activ
-activ[, 2] <- tolower(gsub("_", "", activ[, 2]))
-activ_descrip  <- activ[Y_tr_ts[, 1], 2]
-Y_tr_ts[, 1] <- activ_descrip
-names(Y_tr_ts) <- "act"
+activ[, 2] <- gsub("_", "", activ[, 2])
+Y_tr_ts[, 1] <- activ[Y_tr_ts[, 1], 2]
+ <- activ_descrip
+colnames(Y_tr_ts) <- "act"
 
 # Appropriately labels the data set with descriptive variable names. 
-names(s) <- "subject"
-new_table <- cbind(s, Y_tr_ts, X_tr_ts)
-dim(new_table)
-write.table(new_table, './new_merged_set.txt')
-head(new_table)
-dim(s)
-dim(Y_tr_ts)
-dim(new_table)
+
+colnames(s) <- "subject"
+new_table <- cbind(s, X_Mean_Sd, Y_tr_ts)
+
+# dim(new_table)
+# head(new_table)
+# dim(s)
+# dim(Y_tr_ts)
+# dim(new_table)
 # Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
 
 average <- aggregate(new_table, by=list(activ=new_table$act, s=new_table$subject), mean)
